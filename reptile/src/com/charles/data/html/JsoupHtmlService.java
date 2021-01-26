dfe48f22574685c295057939c33294f2291bb16d
caomei <zhou.wei@huadongmedia.com>
2021-01-26 11:40:51 +0800

    调试

--- reptile/src/com/charles/data/html/JsoupHtmlService.java
+++ reptile/src/com/charles/data/html/JsoupHtmlService.java
@@ -15,13 +15,9 @@ import java.util.List;
 public class JsoupHtmlService {
     public static PreparedStatement preparedStatement;
     public static Connection connection;
-
-
     private static String dj_url = "https://www.idejian.com/search?keyword=";
-
     public static void main(String[] args) throws Exception {
         String sql = DBUtil.addSearchBook;
-
         if (connection != null) {
             preparedStatement.close();
             connection.close();
@@ -33,16 +29,14 @@ public class JsoupHtmlService {
         for (String string : keyword) {
             getSearch(string);
         }
-
     }
-
 
     private static void getSearch(String k) throws Exception {
 
         JsoupUtil.getJsoup(dj_url + k, new JsoupUtil.HttpCallback() {
             @Override
             public void OnSuccess(Document body) throws Exception {
-                html(body,k);
+                html(body, k);
             }
 
             @Override
@@ -50,18 +44,17 @@ public class JsoupHtmlService {
                 System.out.println(msg);
             }
         });
-
     }
 
     private static void html(Document document, String k) {
         Elements postList = document.getElementsByClass("rank_ullist");
         for (int i = 0; i < postList.size(); i++) {
             Elements items_l = postList.get(i).getElementsByClass("items_l");
-            for ( int j = 0 ; j < items_l.size();j++){
-               String newKeyword = items_l.get(j).getElementsByTag("a").first().getElementsByAttributeStarting("alt").attr("alt");
+            for (int j = 0; j < items_l.size(); j++) {
+                String newKeyword = items_l.get(j).getElementsByTag("a").first().getElementsByAttributeStarting("alt").attr("alt");
                 double strSimilarity = CosineSimilarity.getSimilarity(k, newKeyword);
                 if (strSimilarity >= 0.7f && strSimilarity < 1.0) {
-                    System.out.println(strSimilarity * 100 +(k+"===="+newKeyword));
+                    System.out.println(strSimilarity * 100 + (k + "====" + newKeyword));
                 }
             }
         }
